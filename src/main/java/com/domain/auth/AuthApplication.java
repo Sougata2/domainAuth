d4db346a09e966a6961b811a09bf6551b5235af7
sougata <sougatadas@cdac.in>
2025-11-28 16:02:47 +0530

    added mapper dependency.

--- src/main/java/com/domain/auth/AuthApplication.java
+++ src/main/java/com/domain/auth/AuthApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = {"com.domain.auth", "com.domain.mapper"})
 public class AuthApplication {
 
     public static void main(String[] args) {

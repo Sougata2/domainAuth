da2127afe5a70039210671c0496b006f85dbbeb5
sougata <sougata19582001@gmail.com>
2025-12-03 01:00:03 +0530

    added domain exception dependency.

--- src/main/java/com/domain/auth/AuthApplication.java
+++ src/main/java/com/domain/auth/AuthApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication(scanBasePackages = {"com.domain.auth", "com.domain.mapper"})
+@SpringBootApplication(scanBasePackages = {"com.domain.auth", "com.domain.mapper", "com.domain.exception"})
 public class AuthApplication {
 
     public static void main(String[] args) {

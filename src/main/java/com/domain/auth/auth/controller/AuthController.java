f3721ce70cfd4b7eed5d2a89c745d2a3781fbcdc
sougata <sougata19582001@gmail.com>
2025-12-06 23:35:45 +0530

    added new controller verify user

--- src/main/java/com/domain/auth/auth/controller/AuthController.java
+++ src/main/java/com/domain/auth/auth/controller/AuthController.java
@@ -4,19 +4,18 @@ import com.domain.auth.auth.dto.AuthDto;
 import com.domain.auth.auth.dto.LoginDto;
 import com.domain.auth.auth.dto.RegisterDto;
 import com.domain.auth.auth.service.AuthService;
+import com.domain.auth.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
     private final AuthService authService;
+    private final UserService userService;
 
     @PostMapping("/login")
     public ResponseEntity<AuthDto> login(@RequestBody LoginDto dto) {
@@ -27,5 +26,10 @@ public class AuthController {
     public ResponseEntity<Void> register(@RequestBody RegisterDto dto) {
         authService.register(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/verify-user/{username}")
+    public ResponseEntity<String> verifyUser(@PathVariable(value = "username") String email) {
+        return ResponseEntity.ok(userService.findByEmail(email).getDefaultRole().getName());
     }
 }

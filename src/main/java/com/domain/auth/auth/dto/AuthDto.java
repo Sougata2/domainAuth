82d6cff7bbc8b0268452c47dedfe2d5b018cfb80
sougata <sougatadas@cdac.in>
2026-01-20 17:24:44 +0530

    added new field refresh token

--- src/main/java/com/domain/auth/auth/dto/AuthDto.java
+++ src/main/java/com/domain/auth/auth/dto/AuthDto.java
@@ -1,6 +1,7 @@
 package com.domain.auth.auth.dto;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
-public record AuthDto(String email, String token, LocalDateTime expiration) {
+public record AuthDto(String email, String token, UUID refreshToken, LocalDateTime expiration) {
 }

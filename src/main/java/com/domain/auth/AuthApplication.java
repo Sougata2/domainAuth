d373e68a8c8e07fbfe34c25e875293506811bb8e
sougata <sougatadas@cdac.in>
2026-01-20 17:25:07 +0530

    added configuration properties scan

--- src/main/java/com/domain/auth/AuthApplication.java
+++ src/main/java/com/domain/auth/AuthApplication.java
@@ -2,9 +2,11 @@ package com.domain.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
+@ConfigurationPropertiesScan
 @SpringBootApplication(scanBasePackages = {"com.domain.auth", "com.domain.mapper", "com.domain.exception"})
 public class AuthApplication {
 

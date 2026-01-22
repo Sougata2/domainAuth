a15f659e01b94f3ebbb3bae05d97ac05104029e5
sougata <sougatadas@cdac.in>
2026-01-22 17:07:31 +0530

    added new field firstName, lastName in auth dto

--- src/main/java/com/domain/auth/auth/dto/AuthDto.java
+++ src/main/java/com/domain/auth/auth/dto/AuthDto.java
@@ -1,7 +1,12 @@
 package com.domain.auth.auth.dto;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
-public record AuthDto(String email, String token, UUID refreshToken, LocalDateTime expiration) {
+public record AuthDto(
+        String email,
+        String firstName,
+        String lastName,
+        String token,
+        LocalDateTime expiration
+) {
 }
